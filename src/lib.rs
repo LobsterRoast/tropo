128ecabd3f5fc208244dcab4805ab02f256a2b7f
lobsterroast <jekamminga@outlook.com>
2024-11-26 13:43:50 -0500

    Added OpenSSL support

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
 use std::net::TcpStream;
 use std::str;
 use std::io::Read;
+use openssl::ssl::SslStream;
 
 
 pub trait HandleRequest {
     fn handle_request() -> Result<(), Box<dyn std::error::Error>>;
 }
 pub struct HttpRequest {
+    pub uri: String,
+    pub host: String,
+    pub user_agent: String,
+}
+
+pub struct HttpsRequest {
     pub uri: String,
     pub host: String,
     pub user_agent: String,
@@ -40,3 +47,33 @@ impl HttpRequest {
         }
     }
 }
+
+impl HttpsRequest {
+    pub fn new(mut stream: &SslStream) -> Self {
+        let mut byte_data = [0; 1024];
+        let byte_count = stream.read(&mut byte_data).unwrap();
+        let mut parsed_http_data = str::from_utf8(&byte_data[..byte_count]).unwrap().lines();
+        Self {
+            uri: parsed_http_data
+                .next()
+                .take()
+                .unwrap()
+                .to_owned(),
+            host: parsed_http_data
+                .next()
+                .take()
+                .unwrap()
+                .strip_prefix("Host: ")
+                .unwrap()
+                .to_owned(),
+            user_agent: parsed_http_data
+                .next()
+                .take()
+                .unwrap()
+                .strip_prefix("User-Agent: ")
+                .unwrap()
+                .to_owned()
+        }
+    }
+}
+
